1b56a543c17afcdaecebda196f75af6bdbbd7df3
flancien <francois.regis.lancien@gmail.com>
2018-01-31 15:31:03 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('mainCanvas') myCanvas: ElementRef;
+  public context: CanvasRenderingContext2D;
+
+  public ngAfterViewInit(): void {
+    const canvasEl = this.myCanvas.nativeElement;
+    this.context = canvasEl.getContext('2d');
+    console.log(this.context);
+    this.draw();
+  }
+
+  private draw(): void {
+    // this.context.beginPath();
+    // this.context.moveTo(0, 0);
+    // this.context.lineTo(300, 150);
+    // this.context.stroke();
+    let img = new Image();
+    let ctx = this.context;
+    img.src = 'https://sc01.alicdn.com/kf/HTB1NWWlSXXXXXaDXVXX760XFXXXG/200652865/HTB1NWWlSXXXXXaDXVXX760XFXXXG.png';
+    img.onload = function() {
+      ctx.drawImage(img, 0, 0, (img.width) / 4, (img.height) / 4);
+    };
+  }
+
 }
